b31eb4eb01579ef7ea057ccaf4986320c5121930
Deepak00001 <deepakgupt001@gmail.com>
2020-10-11 12:08:48 +0530

    login, signup and javascript

--- js/java.js
+++ js/java.js
@@ -5,3 +5,27 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+  function searchcat() {
+    var input = document.getElementById('search').value;
+    if(input=='Mens'||input=='mens'){
+        location = "mens.html";
+    }
+    else if(input=='Women'||input=='women'){
+        location = "women.html";
+    }
+    else if(input=='Kids'||input=='kids'){
+      location = "kids.html";
+    }
+    else if(input=='furniture'||input=='Furniture'){
+      location = "furniture.html";
+    }
+    else if(input=='shoes'||input=='Shoes'){
+      location = "shoes.html";
+    }
+    else if(input=='electronic'||input=='Electronic'){
+      location = "electronic.html";
+    }
+    else{
+      alert('Search not found');
+    }
+  }
